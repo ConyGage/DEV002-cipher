75d139a9cbfe6b775670551bea2ae7ae9f04dafa
Cony GuGa <constanza.garcia.gtz@gmail.com>
2022-11-14 22:50:24 -0600

    Buenas Intenciones pero no

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,47 @@
 const cipher = {
-  // ...
+  encode: function (offset, string) {//cipher.encode(offset, string): offset es el número de posiciones que queremos mover a la derecha en el alfabeto y string el mensaje (texto) que queremos cifrar.
+    /*//Cambios------------------------------------------------
+    if (string === null) 
+    //Cambios
+    //Cambios
+    if (string === 0) 
+    //Cambios
+    //Cambios
+    if (string === null) 
+    //Cambios//Cambios*/
+    if (!offset) {
+      throw new TypeError("No... Debo establecer un rango de seguridad antes de encriptar el código")
+    }
+    if (!string) {
+      throw new TypeError("¿Cuáles serán las coordenadas..?")
+    }
+    //Cambios------------------------------------------------
+    /*if (!offset) {
+      throw new TypeError("No... Debo establecer un rango de seguridad antes de encriptar el código")
+    }*/
+    //if (!string)
+    //Analizar en ciclo el mensaje a codificar
+    //Establecemos constantes que necesitaremos para codificar.
+    //return ""; //Almacenará el resultado del proceso de cifrar. //Tiene este nombre rebuscado para practicar inglés... ;P
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //Establezco el marco en el cuál me basaré para contar el desplazamiento-Offset. //Estaría más chido dunkeyAlfa, pero no sería muy profesional de mi parte ni lo suficientemente divertido para otras personas que lean el codigo...
+    const offsetPositiv = (offset % 26 + 26) % 26;
+    return string.replace(/[A-Z]/ig, c => alphabet[(alphabet.indexOf(c) + offsetPositiv) % 26]);
+    //message.replace(/[A-Z]/ig, c => theAlfa[(theAlfa.indexOf(c) + code) % 26]);
+    //console.log(this.encode);
+  },
+  decode: function (offset, string) {
+    if (!offset) {
+      throw new TypeError("No... Debo establecer un rango de seguridad antes de encriptar el código")
+    }
+    if (!string) {
+      throw new TypeError("¿Cuáles serán las coordenadas..?")
+    }
+    /*if (!string)
+    return "";*/
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const offsetPositiv = (offset % 26 - 26) % 26;
+    return string.replace(/[A-Z]/ig, c => alphabet[(alphabet.indexOf(c) - offsetPositiv) % 26]);
+  }
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+//npm tesconsole.log(cipher);
+//Inputs Llamado    //Llamamos a los inputs que contendran texto
+const messageConteiner = document.getElementById("introMsn")
+const codeConteiner = document.getElementById("encryptCode")
+const encryptMsnConteiner = document.getElementById("encryptMsn")
+//
+//Botones Llamado   //Llamamos a los botones que accionarán un acto
+const btnEncrypt = document.getElementById("btnEncrypt")
+const btnClear = document.getElementById("btnClear")
+const btnUnEncrypt = document.getElementById("btnUnEncrypt")
+const btnSendMsn = document.getElementById("btnSendMsn")
+//
+//Eventos-Activadores   //Especificamos que eventos se acciónaran, cuándo lo harán y con la función el qué y cómo lo haran
+messageConteiner.addEventListener("keyup", function () {
+    messageConteiner.value = messageConteiner.value.toUpperCase()//this.value = this.value.toUpperCase() "this.value === messangeConteiner.value"
+})
+//
+//
+btnEncrypt.addEventListener("click", function () {
+    //console.log(messageConteiner, codeConteiner) Este console fue aplicado para revisar el error de valor nulo al no haber referencia correcta de Code-code
+    document.getElementById("encryptMsn").value = cipher.encode(codeConteiner.value, messageConteiner.value)
+    //document.getElementById("encryptMsn").value = cipher.encode(codeConteiner.value, messageConteiner.value) CREÍ QUE ERA REDUNDANTE ASÍ QUE LO BLOQUEE Y COMBINÉ EL DOCUMENT.GETELEMNTBYID CON LA FUNCIÓN DE CIPHER...
+})
+btnClear.addEventListener("click", function () {
+    messageConteiner.value = ""
+    codeConteiner.value = ""
+    encryptMsnConteiner.value = ""//Más avanzado, animación de error fatal con un "If"
+})
+btnUnEncrypt.addEventListener("click", function () {
+    document.getElementById("encryptMsn").value = cipher.decode(codeConteiner.value, messageConteiner.value)
+})
+btnSendMsn.addEventListener("click", function () {
+    //window.href=msnReceived.html//Investigar enlace a otro html.
+    //If no hay mensaje en msnConte, error fatal... o, No, debo envier el codigo de las coordenadas a la comandante Rylek
+});
+//
